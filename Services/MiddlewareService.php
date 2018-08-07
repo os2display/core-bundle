c6858379e61ebbd4c0c71ab6f30e4b2bff120af8
Troels Ugilt Jensen <tujensen@gmail.com>
2018-08-07 13:06:07 +0200

    ER-36: Added middleware service

---
+++ Services/MiddlewareService.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Os2Display\CoreBundle\Services;
+
+use Doctrine\ORM\EntityManagerInterface;
+use JMS\Serializer\SerializerInterface;
+use Os2Display\CoreBundle\Entity\Channel;
+use Os2Display\CoreBundle\Entity\Screen;
+use JMS\Serializer\SerializationContext;
+
+class MiddlewareService
+{
+    protected $serializer;
+    protected $entityManager;
+    protected $channelRepository;
+    protected $screenRepository;
+
+    /**
+     * ScreenService constructor.
+     */
+    public function __construct(EntityManagerInterface $entityManager, SerializerInterface $serializer)
+    {
+        $this->entityManager = $entityManager;
+        $this->serializer = $serializer;
+        $this->channelRepository = $entityManager->getRepository(Channel::class);
+        $this->screenRepository = $entityManager->getRepository(Screen::class);
+    }
+
+    public function getScreenArray($screen) {
+        $data = $this->serializer->serialize($screen, 'json', SerializationContext::create()
+            ->setGroups(array('middleware')));
+
+        return json_decode($data);
+    }
+
+    public function getChannelArray($channel) {
+        $data = $this->serializer->serialize($channel, 'json', SerializationContext::create()
+            ->setGroups(array('middleware')));
+
+        return json_decode($data);
+    }
+
+    public function getCurrentScreenArray($screenId) {
+        $screenRepository = $this->entityManager->getRepository('Os2DisplayCoreBundle:Screen');
+
+        $screen = $screenRepository->findOneById($screenId);
+        $screenArray = $this->getScreenArray($screen);
+
+        $result = (object) [
+            'channels' => [],
+            'screen' => $screenArray,
+        ];
+
+        // @TODO: Handles shared channels.
+        foreach ($screen->getChannelScreenRegions() as $channelScreenRegion) {
+            $channel = $channelScreenRegion->getChannel();
+            $region = $channelScreenRegion->getRegion();
+            $channelId = $channel->getId();
+
+            if (!isset($result->channels[$channelId])) {
+                $data = $this->getChannelArray($channel);
+
+                $result->channels[$channelId] = (object) [
+                    'data' => $data->data,
+                    'regions' => [$region],
+                ];
+            }
+            else {
+                $result->channels[$channelId]->regions[] = $region;
+            }
+        }
+
+        return $result;
+    }
+}
